857b044e443e08b277e7f1909670c329e0d68ba9
bryanpham85 <duchungpham12d2@gmail.com>
2017-07-04 11:46:56 +0700

    Caller/Category model
    define model for minal

--- api/models.py
+++ api/models.py
@@ -2,23 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Caller(models.Model):
-	"""
-	Description: a phone number is a caller at global of app
-	"""
-	COUNTRY_CODE = (
-		('VN(+84)', '+84'),
-	)
-	callerId = models.AutoField(primary_key=True)
-	country_code = models.CharField(max_length = 5, choices=COUNTRY_CODE, null=False)
-	caller_number = models.CharField(max_length = 11, null=False)
-	registerred_date = models.DateTimeField(auto_now_add=True)
-	registerred_by = models.ForeignKey('Device')
-
-	class Meta:
-		ordering = ('registerred_date',)
-
-class Device(models.Model):
+class Registered_Device(models.Model):
 	"""
 	Devices install AnnoyingCaller app
 	"""
@@ -33,5 +17,55 @@ class Device(models.Model):
 	installed_date = models.DateTimeField(auto_now_add=True)
 
 	class Meta:
+		db_table = ('ac_registered_device')
 		ordering = ('installed_date',)
 
+class Caller(models.Model):
+	"""
+	Description: a phone number is a caller at global of app
+	"""
+	COUNTRY_CODE = (
+		('VN(+84)', '+84'),
+	)
+	callerId = models.AutoField(primary_key=True)
+	country_code = models.CharField(max_length = 5, choices=COUNTRY_CODE, null=False)
+	caller_number = models.CharField(max_length = 11, null=False)
+	registered_date = models.DateTimeField(auto_now_add=True)
+	registered_by = models.ForeignKey(Registered_Device)
+
+	class Meta:
+		db_table = ('ac_caller')
+		ordering = ('registered_date',)
+		indexes = [
+			models.Index(fields=['country_code', 'caller_number'], name='caller_index')
+		]
+
+
+
+class Category(models.Model):
+	"""
+	Global used cross platform
+	"""
+	CATEGORY_TYPE = (
+			('Private', 1),
+			('Global', 2),
+		)
+	name = models.CharField(max_length=100, null=False, unique=True)
+	category_type = models.IntegerField(null=False, choices=CATEGORY_TYPE)
+	created_date = models.DateTimeField(auto_now_add=True)
+
+	class Meta:
+		db_table = ('ac_category')
+		ordering = ('created_date',)
+
+class Caller_Categories(models.Model):
+	"""
+	The caller marked into category by owner from device, this table only count the number of report time from community
+	"""
+	callerId = models.ForeignKey('Caller', null=False)
+	globalCategoryId = models.ForeignKey('Category', null=False)
+	report_count = models.IntegerField(default=0)
+
+	class Meta:
+		db_table = ('ac_caller_category')
+		index_together = ['callerId', 'globalCategoryId']
