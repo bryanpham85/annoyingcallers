b7308472681c491c0c1bca4f1a0656f6f6ff048a
bryanpham85 <duchungpham12d2@gmail.com>
2017-07-13 13:29:44 +0700

    update URL for api with "api/" prefix

--- api/urls.py
+++ api/urls.py
@@ -3,11 +3,11 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from api import views
 
 urlpatterns = [
-	url(r'^callers/$', views.CallerList.as_view()),
-	url(r'^callers/(?P<pk>[0-9]+)/$', views.CallerDetail.as_view()),
-	url(r'^categories/$', views.CategoryList.as_view()),
-	url(r'^device/$', views.Registered_DeviceList.as_view()),
-	url(r'^device/(?P<deviceId>[a-z0-9]+)/$', views.Registered_DeviceDetail.as_view()),
+	url(r'^api/callers/$', views.CallerList.as_view()),
+	url(r'^api/callers/(?P<pk>[0-9]+)/$', views.CallerDetail.as_view()),
+	url(r'^apicategories/$', views.CategoryList.as_view()),
+	url(r'^api/device/$', views.Registered_DeviceList.as_view()),
+	url(r'^api/device/(?P<deviceId>[a-z0-9]+)/$', views.Registered_DeviceDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
